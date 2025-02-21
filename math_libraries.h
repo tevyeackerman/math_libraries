2682a5a84cecf308191cc11381bf7cf5f1641c99
tevyeackerman <tevye.projects@gmail.com>
2025-02-21 23:49:36 +0200

    connected files

---
+++ math_libraries.h
@@ -0,0 +1,6 @@
+#ifndef MATH_LIBRARIES_H
+#define MATH_LIBRARIES_H
+
+#include "numerical_analysis_lib/build/numerical_methods.h"
+
+#endif // MATH_LIBRARIES_H
---
+++ numerical_analysis_lib/build/numerical_methods.h
@@ -0,0 +1,6 @@
+#ifndef NUMERICAL_METHODS_H
+#define NUMERICAL_METHODS_H
+
+#include "solutions_of_equations_in_one_variable/solutions_of_equations_in_one_variable.h"
+
+#endif // NUMERICAL_METHODS_H
